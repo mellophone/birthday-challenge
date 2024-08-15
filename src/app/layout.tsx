6a7a6b2fd7c9c1deaae0ed657f3383675fe81beb
Ben Key <54153434+mellophone@users.noreply.github.com>
2024-08-15 10:14:24 -0500

    Minor meta data tweaks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <title>{`Kelsey's Birthday Challenge`}</title>
+      <head>
+        <title>{`Kelsey's Birthday Challenge`}</title>
+        <meta name="theme-color" content="darkgray" />
+        <meta property="og:url" content="/" />
+        <meta property="og:title" content={`Kelsey's Birthday Challenge`} />
+        <meta property="og:type" content="website" />
+      </head>
       <body>{children}</body>
     </html>
   );
